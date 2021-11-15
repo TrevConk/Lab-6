7e6de2d005768c51c86d076a17cc60e1bbf0af2b
TrevConk <trevorconklin@gmail.com>
2021-11-15 15:47:10 -0500

    updated name

--- lab6.py
+++ lab6.py
@@ -3,12 +3,12 @@ from led8x8 import LED8x8
 
 columnDataPin, rowDataPin, latchPin, clockPin = 23, 21, 24, 25
 
-display = LED8x8(columnDataPin, rowDataPin, latchPin, clockPin)
+currentDisplay = LED8x8(columnDataPin, rowDataPin, latchPin, clockPin)
 
 try:
     while True:
         for row in range(8):
-            display.display(row)
+            currentDisplay.display(row)
             print(row)
             time.sleep(.4)
 except Exception as e:
