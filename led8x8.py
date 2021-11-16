c59da9e8911f59f1151112dc53e7b62b13fdc886
TrevConk <trevorconklin@gmail.com>
2021-11-15 19:32:26 -0500

    large update test questionable if will work

--- led8x8.py
+++ led8x8.py
@@ -9,22 +9,17 @@ class LED8x8:
     pattern = [0b00111100, 0b01000010, 0b10100101, 0b10000001, 0b10100101, 0b10011001, 0b01000010, 0b00111100]
     rowsern = [0b10000000, 0b01000000, 0b00100000, 0b00010000, 0b00001000, 0b00000100, 0b00000010, 0b00000001]
 
-    def __init__(self, columnData, latch, clock):
-        self.shifterColumn = Shifter(columnData, latch, clock)
+    def __init__(self, columnDataPin, rowDataPin, latchPin, clockPin):
+        self.shifter = Shifter(columnDataPin, rowDataPin, latchPin, clockPin)
         
-    def display(self):
-        for n in range(len(LED8x8.pattern)):
-            self.shifterColumn.shiftByte(LED8x8.pattern[n])
-            self.shifterColumn.shiftByte(1 << (n))
-            self.shifterColumn.latch()
-        #self.shifterRow.shiftByte(0b10000000)
-        #self.shifterColumn.shiftByte(0b00111100)
-        print(LED8x8.pattern[n])
+    def display(self, num):
+        self.shifter.shiftByte(LED8x8.pattern[num],LED8x8.rowsern[num])
     
 
 
-theDisplay = LED8x8(LED8x8.columnDataPin, LED8x8.latchPin, LED8x8.clockPin)
+theDisplay = LED8x8(LED8x8.columnDataPin, LED8x8.rowDataPin, LED8x8.latchPin, LED8x8.clockPin)
 
 while True:
-  theDisplay.display()
-  time.sleep(.001)
+  for n in range(LED8x8.pattern):
+      theDisplay.display(n)
+      time.sleep(.001)
